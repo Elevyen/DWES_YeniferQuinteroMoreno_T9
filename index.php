4bc722296da48e3465aee0ce6950127b54e2e2ee
yeniferquintero <yen.quimo@gmail.com>
2026-02-03 18:35:31 +0100

    RA9_d commit git

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>DWES T9 Git</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        <h1>DWES Unidad 9: Bienvenidos a Git</h1>
+    </body>
+</html>
